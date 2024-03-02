b09a1fa6ac31ec925325ece3e19d97d11ba3869f
frank <frank@js.ie>
2024-03-02 18:23:44 +0000

    Cleanup for version 0.0.5

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const isString: (str: any) => boolean;
-export declare const isObject: (obj: any) => boolean;
-export declare const isArray: (arr: any) => boolean;
-export declare const isNonEmptyString: (str: any) => boolean;

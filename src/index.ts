73668ab7597d9d9ec775f4b1b449c49937d55b8b
frank <frank@js.ie>
2024-03-16 18:01:50 +0000

    Added testing

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 export const isString = (str: any) =>
 	typeof str === "string" || str instanceof String;
 export const isObject = (obj: any) =>
-	typeof obj === "object" || obj instanceof Object;
+	typeof obj === "object" && obj instanceof Object;
 export const isArray = (arr: any) => arr instanceof Array;
 export const isNonEmptyString = (str) => isString(str) || str.length !== 0;
 
 export const merged = function () {
-	return [...arguments].reduce(
+	const args = Array.prototype.slice.call(arguments);
+	return [...args].reduce(
 		(arr, next) => (!isArray(next) ? arr : [...arr, ...next]),
 		[]
 	);
---
+++ src/test.js
@@ -0,0 +1,36 @@
+const utils = require('../lib');
+
+test('has isString function', () => {
+	expect(utils.isString instanceof Function).toBe(true);
+})
+test('has isObject function', () => {
+	expect(utils.isObject instanceof Function).toBe(true);
+})
+test('has isArray function', () => {
+	expect(utils.isArray instanceof Function).toBe(true);
+})
+test('has isNonEmptyString function', () => {
+	expect(utils.isNonEmptyString instanceof Function).toBe(true);
+})
+test('has merged function', () => {
+	expect(utils.merged instanceof Function).toBe(true);
+})
+
+test('isString function does not consider null to be a string', () => {
+	expect(utils.isString(null)).toBe(false);
+})
+test('isObject function does not consider null to be an object', () => {
+	expect(utils.isObject(null)).toBe(false);
+})
+test('isArray function does not consider object to be an array', () => {
+	expect(utils.isArray({})).toBe(false);
+})
+test('isNonEmptyString function does not consider empty array to be a string', () => {
+	expect(utils.isNonEmptyString([])).toBe(false);
+})
+
+test('merged returns different array object even if only single array passed to it', () => {
+	const testArray = ['Sample'];
+	expect(utils.merged(testArray) === testArray).toBe(false);
+})
+

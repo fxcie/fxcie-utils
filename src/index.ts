2ee31edcdf06e9a4fdbadb281472f6fd267d342b
frank <frank@js.ie>
2024-03-16 15:37:03 +0000

    Added merged function

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,10 @@ export const isObject = (obj: any) =>
 	typeof obj === "object" || obj instanceof Object;
 export const isArray = (arr: any) => arr instanceof Array;
 export const isNonEmptyString = (str) => isString(str) || str.length !== 0;
+
+export const merged = function () {
+	return [...arguments].reduce(
+		(arr, next) => (!isArray(next) ? arr : [...arr, ...next]),
+		[]
+	);
+};

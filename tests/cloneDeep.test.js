560b12948b8c77e6a1bdf2ad2eedb4cc5029babe
frank <frank@js.ie>
2024-03-17 11:02:50 +0000

    Correcting cloneDeep tests for string objects

--- tests/cloneDeep.test.js
+++ tests/cloneDeep.test.js
@@ -14,10 +14,10 @@ test('cloneDeep returns value if a non-object value is supplied', () => {
 	expect(utils.cloneDeep('test') === 'test').toBe(true);
 	expect(utils.cloneDeep(5) === 5).toBe(true);
 })
-test('cloneDeep returns string primitive if a string object is supplied', () => {
+test('cloneDeep returns new string object if a string object is supplied', () => {
 	const testValue = 'test';
 	const testString = new String(testValue);
-	expect(utils.cloneDeep(testString) === testValue).toBe(true);
-	expect(typeof utils.cloneDeep(testString) === 'string').toBe(true);
-	expect(utils.cloneDeep(testString) instanceof Object).toBe(false);
+	expect(utils.cloneDeep(testString) === testString).toBe(false);
+	expect(typeof utils.cloneDeep(testString) === 'object').toBe(true);
+	expect(utils.cloneDeep(testString) instanceof Object).toBe(true);
 })

dcf19a5e357ac60c71b1dfdb38e6f3999c300086
frank <frank@js.ie>
2024-05-06 03:56:48 +0100

    JSON to/from Array/Object

--- src/index.ts
+++ src/index.ts
@@ -60,3 +60,52 @@ export function findRecord(arr: [], field: string, value: any) {
 export function escapeRegExp(str) {
 	return str.replace(/[[\]*+?{}.()^$|\\-]/g, "\\$&");
 }
+
+export function jsonToArray(jsonArray: string) {
+	var s = [];
+	try {
+		s = JSON.parse(jsonArray);
+	} catch (e) {
+		s = [];
+	}
+	return s;
+}
+
+export function arrayToJson(array: any[]) {
+	var s = "[]";
+	if (!(array instanceof Array)) return s;
+	try {
+		s = JSON.stringify(array);
+	} catch (e) {
+		s = "[]";
+	}
+	return s;
+}
+
+export function jsonToObject(jsonObject: string) {
+	var s = {};
+	try {
+		s = JSON.parse(jsonObject);
+	} catch (e) {
+		s = {};
+	}
+	return s;
+}
+
+export function objectToJson(
+	object: Record<string, any>,
+	pretty: boolean = false
+) {
+	var s = "{}";
+	if (!(object instanceof Object)) return s;
+	try {
+		s = JSON.stringify(
+			object,
+			pretty ? null : undefined,
+			pretty ? 2 : undefined
+		);
+	} catch (e) {
+		s = "{}";
+	}
+	return s;
+}
